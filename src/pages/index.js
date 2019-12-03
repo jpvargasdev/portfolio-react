d606ba673e344bddc0c01d4d69021c0412fc782d
Juan Vargas <vargasm.jp@gmail.com>
2019-12-03 14:07:24 -0500

    Second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// api
+import { getHomeData } from 'api';
+
 // components
 import { Seo } from "components/Base";
 import { Layout } from "components/Elements"
@@ -10,16 +13,21 @@ import Home from "templates/Home";
 // styles
 import '../styles/index.scss';
 
-const IndexPage = () => (
-  <main>
-    <Layout
-      showParticles
-      showMenu
-      showSocial
-    />
-    <Seo title="Home" />
-    <Home />
-  </main>
-);
+const IndexPage = () => {
+
+  const { json } = getHomeData();
+
+  return (
+    <main>
+      <Layout
+        showParticles
+        showMenu
+        showSocial
+      />
+      <Seo title="Home" />
+      <Home description={json} />
+    </main>
+  );
+};
 
 export default IndexPage;
--- src/templates/Home/index.js
+++ src/templates/Home/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
+// components
+import renderRichText from 'components/Base/RichText';
+
 // styles
 import './home.scss'
 
 const Home = ({
   description
-}) => (
-  <div className='home_container'>
-    <p>Hola Juan</p>
-    <button>Go to my projects</button>
-  </div>
-);
+}) => {
+  const RichText = renderRichText(description);
+  return (
+    <div className='home_container'>
+      <button>Go to my projects</button>
+    </div>
+  )
+};
 
 Home.propTypes = {
   description: PropTypes.string,
