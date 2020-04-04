80be6e690e72b9076534f2ed497d84c2d0d7297e
Juan Vargas <juavargas@ML-C02T341ZH03Q.utg.uvn.net>
2020-04-04 08:01:34 -0500

    add new elements

---
+++ src/components/Elements/AppButton/index.js
@@ -0,0 +1,50 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import PropTypes from "prop-types";
+
+// components
+import Arrow from "react-ionicons/lib/IosArrowForward";
+import Touchable from "../../Base/Touchable";
+
+// style
+import "./appButton.scss";
+
+// constants
+const DEFAULT_ICON_PROPS = {
+  fontSize: "20px",
+  color: "white"
+};
+
+const renderIcon = () => <Arrow {...DEFAULT_ICON_PROPS} />;
+
+const Button = ({ text, variant, animation, onClick, useIcon }) => (
+  <Touchable
+    className={`button button-${variant}`}
+    type="button"
+    animation={animation}
+    onClick={onClick}
+  >
+    <>
+      {text}
+      <div style={{ width: 5 }} />
+      {useIcon && renderIcon()}
+    </>
+  </Touchable>
+);
+
+Button.propTypes = {
+  text: PropTypes.string,
+  variant: PropTypes.string,
+  animation: PropTypes.string,
+  onClick: PropTypes.func,
+  useIcon: PropTypes.bool
+};
+Button.defaultProps = {
+  text: "",
+  variant: "default",
+  animation: "zoom",
+  useIcon: false,
+  onClick: () => true
+};
+
+export default Button;
--- src/templates/Project/index.js
+++ src/templates/Project/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -5,6 +7,12 @@ import PropTypes from "prop-types";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Chip from "components/Base/Chip";
+import Touchable from "components/Base/Touchable";
+
+// images
+import AppleStoreBadge from "../../assets/apple-store.svg";
+import GooglePlayBadge from "../../assets/play-store.svg";
 
 // styles
 import "./project.scss";
@@ -17,31 +25,32 @@ const slideSettigs = {
   autoplay: true,
   autoplaySpeed: 2000,
   slidesToScroll: 1,
-  cssEase: "linear",
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 2,
         infinite: true,
-        dots: true
+        dots: true,
       }
     },
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 2,
         infinite: true,
-        dots: true
+        dots: true,
+        centerMode: true
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        centerMode: true
       }
     }
   ]
@@ -58,33 +67,40 @@ const Project = ({ element }) => {
     webUrl,
   } = element;
 
-  console.log(technologies);
+  console.log(element);
   return (
     <div className="project_container">
       <h1 className="project_h1">{title}</h1>
-      <Slider
-        {...slideSettigs}
-        arrows
-        className="project_container_image"
-      >
-        {
-          images.map(element => (
-            <div>
-              <img className="project_image" src={element.file.url} />
-            </div>
-          ))
-        }
+      <h2 className="project_p">{resume}</h2>
+      <div className="project_buttons">
+        <Touchable animation="zoom">
+          <h2> Android App </h2>
+          <GooglePlayBadge />
+        </Touchable>
+        <Touchable animation="zoom">
+          <h2> iPhone App </h2>
+          <AppleStoreBadge />
+        </Touchable>
+      </div>
+      <Slider {...slideSettigs} arrows className="project_container_image">
+        {images.map(image => (
+          <div>
+            <img
+              className="project_image"
+              alt={image.file.url}
+              src={image.file.url}
+            />
+          </div>
+        ))}
       </Slider>
       <section className="project_description">
         <h2 className="project_h2">Technologies:</h2>
         <ul className="project_list_technologies">
-          {
-            technologies.map(element => (
-              <li>
-                {element}
-              </li>
-            ))
-          }
+          {technologies.map(technology => (
+            <li>
+              <Chip>{technology}</Chip>
+            </li>
+          ))}
         </ul>
       </section>
     </div>
